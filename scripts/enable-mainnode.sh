f1429c1e1190141198af48db42d7795e70b82864
zelbov <jimborecard@gmail.com>
2024-07-01 20:26:20 +0300

    Additional labeling for local dev node to support all kinds of objects to be deployed locally

--- scripts/enable-mainnode.sh
+++ scripts/enable-mainnode.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 # Enables pods scheduling on control plane node for development environment
+# Also marks current local node with all necessary labels to deploy any kind of objects
+# that require specific node capabilities, like persistent storage nodes
 
 NODE_NAME=$(hostname)
 
-kubectl taint node $(hostname) node-role.kubernetes.io/control-plane:NoSchedule-
+# Untaint local main node to allow local deployments
+kubectl taint node $NODE_NAME node-role.kubernetes.io/control-plane:NoSchedule-
+
+# Mark with persistence-managed-replicas capability to allow deployment of according services
+# e.g. Redis or RabbitMQ, as they manage storage sync and failover by themselves across different storage instances
+kubectl label nodes $NODE_NAME persistence-managed-replicas
+
+# Mark with persistence-nfs capability to allow deployment of singleton storages that are accessed by cluster though network
+# Such as multiple instances of application that are claiming the same NFS-type PV
+kubectl label nodes $NODE_NAME persistence-nfs-single
+
+# Mark with relay-proxy capability to allow deployment of traffic relays accessed by clients
+# in order to achieve traffic routing optimization (e.g. globally distributed traffic proxies under anycast DNS)
+kubectl label nodes $NODE_NAME relay-proxy
