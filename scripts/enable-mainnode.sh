d036c787a1ae9da6c725de6e6b8df6ab1a969f3f
zelbov <jimborecard@gmail.com>
2024-07-05 23:30:35 +0300

    Configure Helm chart and Redis cluster deployment

--- scripts/enable-mainnode.sh
+++ scripts/enable-mainnode.sh
@@ -11,12 +11,12 @@ kubectl taint node $NODE_NAME node-role.kubernetes.io/control-plane:NoSchedule-
 
 # Mark with persistence-managed-replicas capability to allow deployment of according services
 # e.g. Redis or RabbitMQ, as they manage storage sync and failover by themselves across different storage instances
-kubectl label nodes $NODE_NAME persistence-managed-replicas
+kubectl label nodes $NODE_NAME persistence-managed-replicas=true --overwrite
 
 # Mark with persistence-nfs capability to allow deployment of singleton storages that are accessed by cluster though network
 # Such as multiple instances of application that are claiming the same NFS-type PV e.g. raw file storages
-kubectl label nodes $NODE_NAME persistence-nfs-single
+kubectl label nodes $NODE_NAME persistence-nfs-single=true --overwrite
 
 # Mark with relay-proxy capability to allow deployment of traffic relays accessed by clients
 # in order to achieve traffic routing optimization (e.g. globally distributed traffic proxies under anycast DNS)
-kubectl label nodes $NODE_NAME relay-proxy
+kubectl label nodes $NODE_NAME relay-proxy=true --overwrite
