a4b7d77dc2a0b5aa362060441bbc02de30557ded
zelbov <jimborecard@gmail.com>
2024-07-18 23:18:32 +0300

    Partial: Redis SagaResponseChannelAdapter

---
+++ src/modules/saga/adapters/channels/Redis.SagaResponseChannel.adapter.ts
@@ -0,0 +1,161 @@
+import { AsyncLock } from "../../../async-lock/AsyncLocks";
+import { formatErrorRecursive } from "../../helpers/RecursiveErrorsFormatting.helper";
+import { DefaultSagaRequestType, DefaultSagaResponseType } from "../../types/Saga.types";
+import { SagaResponseChannelAdapter } from "../../types/SagaResponseChannelAdapter.types";
+import { RedisClientOptions, RedisClientType, RedisDefaultModules, RedisFunctions, RedisModules, RedisScripts, createClient } from 'redis'
+
+interface RedisResponseChannelAdapterCredentials
+extends RedisClientOptions<RedisModules, RedisFunctions, RedisScripts> {}
+
+export class RedisSagaResponseChannelAdapter 
+implements SagaResponseChannelAdapter<RedisResponseChannelAdapterCredentials> {
+
+    private credentials?: RedisResponseChannelAdapterCredentials
+    private connection_lock: AsyncLock
+    private redis_publisher_connection?: RedisClientType<RedisDefaultModules & RedisModules, RedisFunctions, RedisScripts>
+    private redis_subscriber_connection?: RedisClientType<RedisDefaultModules & RedisModules, RedisFunctions, RedisScripts>
+    private disposed: boolean = false
+
+    constructor(){
+        this.connection_lock = new AsyncLock()
+    }
+
+    setupCredentials(credentials: RedisResponseChannelAdapterCredentials): RedisResponseChannelAdapterCredentials {
+        this.credentials = credentials
+        return credentials
+    }
+
+    //TODO: retries
+    async connection(): Promise<void> {
+
+        if(!this.credentials) throw new Error('Adapter requires credentials specified before establishing connection')
+        if(this.disposed) throw new Error('Adapter connection disposed')
+
+        await this.connection_lock.acquire()
+
+        for(let connectionType of
+            ['redis_publisher_connection', 'redis_subscriber_connection'] as 
+            ['redis_publisher_connection', 'redis_subscriber_connection']
+        ) if(!this[connectionType]) {
+
+            try {
+
+                const connection = createClient(this.credentials)
+
+                await new Promise<void>((resolve, reject) => {
+
+                    connection.on('connect', () => {
+                        this[connectionType] = connection
+                        resolve()
+                    })
+
+                    connection.on('error', (err) => {
+
+                        if(!this[connectionType] && !this.disposed) reject(err)
+                        this[connectionType] = undefined
+
+                    })
+
+                    connection.connect()
+
+                })
+
+            } catch(ex) {
+
+                this.connection_lock.release()
+                throw ex
+
+            }
+
+        }
+        
+        this.connection_lock.release()
+
+    }
+
+    async publishResponse<SagaResponse extends DefaultSagaResponseType>(
+        response: SagaResponse
+    ): Promise<void> {
+
+        if(!response.request_id) throw new Error('A published response does not refer to any unique request ID')
+        
+        const str = JSON.stringify({response}),
+            channel = response.request_id+'.res'
+
+        await this.connection()
+
+        await this.redis_publisher_connection!.publish(channel, str)
+
+    }
+
+    async publishError<SagaErrorResponseType extends DefaultSagaResponseType>(
+        letter: SagaErrorResponseType,
+        error: Error
+    ): Promise<void> {
+
+        if(!letter.request_id) throw new Error('A published error does not refer to any unique request ID')
+        
+        const str = JSON.stringify({ response: letter, error: formatErrorRecursive(error) }),
+            channel = letter.request_id+'.err'
+
+        await this.connection()
+
+        await this.redis_publisher_connection!.publish(channel, str)
+        
+    }
+
+    async subscribeToResponse<SagaResponse extends DefaultSagaResponseType>(
+        request_id: string,
+        callback: (response: SagaResponse) => Promise<void>
+    ): Promise<void> {
+
+        await this.connection()
+
+        const channel = request_id+'.res'
+
+        await this.redis_subscriber_connection!.subscribe(channel, async (msg) => {
+
+            const { response } = JSON.parse(msg) as { response: SagaResponse }
+
+            await callback(response)
+
+        })
+
+    }
+
+    async subscribeToError<SagaErrorResponseType extends DefaultSagaResponseType>(
+        request_id: string,
+        callback: (response: SagaErrorResponseType, error: Error) => Promise<void>
+    ): Promise<void> {
+        
+        await this.connection()
+
+        const channel = request_id+'.err'
+
+        await this.redis_subscriber_connection!.subscribe(channel, async (msg) => {
+
+            const { response, error } = JSON.parse(msg) as { response: SagaErrorResponseType, error: Error }
+
+            await callback(response, error)
+
+        })
+
+    }
+
+    async disposeSubscriptions(request_id: string): Promise<void> {
+        
+        await this.connection()
+
+        await this.redis_subscriber_connection!.unsubscribe(request_id+'.res')
+        await this.redis_subscriber_connection!.unsubscribe(request_id+'.err')
+
+    }
+
+    async dispose(): Promise<void> {
+        if(this.disposed) return
+        this.disposed = true
+        if(this.redis_publisher_connection) this.redis_publisher_connection.quit()
+        if(this.redis_subscriber_connection) this.redis_subscriber_connection.quit()
+    }
+
+}
---
+++ test/integration/modules/RedisResponseChannelAdapter.test.ts
@@ -0,0 +1,142 @@
+import 'mocha'
+import { expect } from 'chai'
+import { RedisSagaResponseChannelAdapter } from '../../../src/modules/saga/adapters/channels/Redis.SagaResponseChannel.adapter'
+import { randomUUID } from 'crypto'
+import { DefaultSagaResponseType } from '../../../src/modules/saga/types/Saga.types'
+import { StubSagaQueuesAdapter } from '../../../src/modules/saga/adapters/queues/Stub.SagaQueues.adapter'
+
+describe('RedisSagaResponseChannelAdapter integration tests', () => {
+
+    const credentials : ReturnType<RedisSagaResponseChannelAdapter['setupCredentials']> = {
+        url: process.env.REDIS_CONNECTION_URL
+    }
+
+    describe('Connectivity tests', () => {
+
+        const adapter = new RedisSagaResponseChannelAdapter()
+
+        after(async function(){
+            await adapter.dispose()
+        })
+
+        it('Setup credentials: should succeed', () => {
+
+            adapter.setupCredentials(credentials)
+
+        })
+
+        it('Establish a connection: should succeed', async function(){
+
+            await adapter.connection()
+
+        })
+
+        it('Close internal connection: should re-establish upon second connection awaiter call', async function() {
+
+            adapter['redis_publisher_connection']!.quit()
+
+            await adapter.connection()
+
+        })
+
+        it('Dispose adapter & try connecting again: should error', async function() {
+
+            await adapter.dispose()
+
+            try {
+
+                await adapter.connection()
+
+            } catch(ex) {
+
+                expect((ex as Error).message).contain('Adapter connection disposed')
+
+            }
+
+        })
+        
+    })
+
+    describe('Publish & subscribe tests', () => {
+
+        const adapter = new RedisSagaResponseChannelAdapter()
+        adapter.setupCredentials(credentials)
+
+        after(async function() {
+            await adapter.dispose()
+        })
+
+        it('Publish response & obtain by response subscription: should succeed', async function(){
+
+            const request_id = randomUUID()
+
+            const response = await new Promise<DefaultSagaResponseType & { data: { foo: string } }>(async (resolve) => {
+
+                await adapter.subscribeToResponse<DefaultSagaResponseType & { data: { foo: string } }>(
+                    request_id,
+                    async (response) => {
+                        await adapter.disposeSubscriptions(response.request_id)
+                        resolve(response)
+                    }
+                )
+
+                await adapter.publishResponse({ request_id, data: { foo: 'bar' }})
+
+            })
+
+            expect(response.request_id).eq(request_id)
+            expect(response.data!.foo).eq('bar')
+
+        })
+
+        it('Publish error & obtain by error response subscription: should succeed', async function() {
+
+            const request_id = randomUUID()
+
+            const { response, error } : {
+                response: DefaultSagaResponseType & { data: { foo: string } },
+                error: Error
+            } = await new Promise(async (resolve) => {
+
+                await adapter.subscribeToError<DefaultSagaResponseType & { data: { foo: string } }>(
+                    request_id,
+                    async (response, error) => {
+                        await adapter.disposeSubscriptions(response.request_id)
+                        resolve({ response, error })
+                    }
+                )
+
+                await adapter.publishError({ request_id, data: { foo: 'bar' }}, new Error('Should error here'))
+
+            })
+
+            expect(response.request_id).eq(request_id)
+            expect(response.data!.foo).eq('bar')
+            expect(error).not.null.and.not.undefined
+            expect(error.message).eq('Should error here')
+
+        })
+
+    })
+
+    describe('SagaOperator interopability tests', () => {
+
+        const adapter = new RedisSagaResponseChannelAdapter()
+        adapter.setupCredentials(credentials)
+
+        const queueAdapter = new StubSagaQueuesAdapter()
+        queueAdapter.setupCredentials({})
+
+        after(async function(){
+
+            await adapter.dispose()
+
+        })
+
+        it('Create one-step saga context, run and attach to SagaOperator')
+        it('Send task to saga through Operator that uses Redis adapter: should obtain response from pub channel')
+        it('Send erroneous task to saga: should obtain error response from error pub channel')
+
+    })
+
+})
